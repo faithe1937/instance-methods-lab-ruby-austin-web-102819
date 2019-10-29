a1d7e2b751ec059cfe006732172325f4b0f5bc76
Faith Janicek <faithe1937@gmail.com>
2019-10-29 16:22:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,3 +4,18 @@ end
 def bark
   puts "Woof!"
 end
+
+def sit 
+  puts "The Dog is sitting"
+end 
+
+class Person
+end 
+
+def talk
+  puts "Hello Wold!"
+end
+
+def walk 
+  puts "The Person is walking"
+end 
